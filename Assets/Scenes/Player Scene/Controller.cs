c52650b66af860de49a03303c878591f820d6183
CPU <claudio.pucci@gmail.com>
2022-08-15 19:52:04 +0200

    Basic env for player movement

--- Assets/Scenes/Player Scene/Controller.cs
+++ Assets/Scenes/Player Scene/Controller.cs
@@ -5,16 +5,21 @@ public class Controller : MonoBehaviour {
 
   public Animator anim;
   public float walkDir = 0;
-
+  public float angle = 0;
 
   private void Update() {
     float x = Input.GetAxis("Horizontal");
-    if (x == 0) walkDir *= 1 - 5 * Time.deltaTime;
+    if (x == 0) walkDir *= 1 - 25 * Time.deltaTime;
     else {
       walkDir += x * Time.deltaTime * 2;
       walkDir = Mathf.Clamp(walkDir, -1f, 1f);
     }
     anim.SetFloat("Movement", walkDir);
+
+    if (Mathf.Abs(walkDir) > .1f) {
+      angle += walkDir * .1f;
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), 10 * Time.deltaTime);
+    }
   }
 
 
