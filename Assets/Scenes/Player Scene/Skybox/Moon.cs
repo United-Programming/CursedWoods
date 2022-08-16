1d976aa9cef654935aeb1aabcfc5d9eb37045da9
CPU <claudio.pucci@gmail.com>
2022-08-16 21:19:40 +0200

    Improved Skybox
    Light is not directly from the moon, and the moon position and size can be controlled.

---
+++ Assets/Scenes/Player Scene/Skybox/Moon.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class Moon : MonoBehaviour {
+  Transform cam;
+  public Vector3 direction;
+  public float distance;
+  public float scale;
+  Vector3 scalev;
+
+  private void Start() {
+    cam = Camera.main.transform;
+    scalev = new Vector3(scale, scale, scale);
+    transform.localScale = scalev;
+  }
+  private void Update() {
+
+    transform.position = cam.position + direction * distance;
+    transform.LookAt(cam.position);
+    scalev.x = scale;
+    scalev.y = scale;
+    transform.localScale = scalev;
+  }
+}
