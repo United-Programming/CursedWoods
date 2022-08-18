ee5657fa0a049fa6caa96afc98a554b752951a5c
Aenimus <47415099+Aenimus@users.noreply.github.com>
2022-08-18 22:51:36 +0100

    Intro main menu (#2)
    * Hooking main menu into Intro scene
    
    * Fix UI bugs
    
    * Removing unused import

---
+++ Assets/Scenes/Intro Scene/Scripts/MainMenuButtonController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler
+{
+    [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private Button button;
+    [SerializeField] private GameObject selector;
+    [SerializeField] private Button defaultButton;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (MainMenuController.CurrentlySelected != null && MainMenuController.CurrentlySelected != this)
+        {
+            MainMenuController.CurrentlySelected.ShowSelector(false);
+        }
+
+        selector.SetActive(true);
+        ShowSelector(true);
+        MainMenuController.CurrentlySelected = this;
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        defaultButton.Select();
+        ShowSelector(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.Select();
+    }
+
+    public void ShowSelector(bool boolean)
+    {
+        selector.SetActive(boolean);
+    }
+
+    private IEnumerator CheckSelection()
+    {
+        yield return null;
+        if (eventSystem.currentSelectedGameObject != null &&
+            eventSystem.currentSelectedGameObject != MainMenuController.LastButton)
+        {
+            yield break;
+        }
+
+        defaultButton.Select();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckSelection());
+    }
+}
---
+++ Assets/Scenes/Intro Scene/Scripts/MainMenuController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public static MainMenuButtonController CurrentlySelected { get; set; }
+
+    public static GameObject LastButton { get; private set; }
+
+    private static readonly int ShowMenu = Animator.StringToHash("showMenu");
+
+    [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool isTitleSequenceFinished;
+    [SerializeField] private bool isOnMenu;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private Button defaultMainMenuButton;
+    [SerializeField] private GameObject optionsButton;
+    [SerializeField] private GameObject returnButton;
+
+    public void StartNewGame()
+    {
+        Debug.Log("Start a new game!");
+        // starts a new game
+    }
+
+    public void ShowOptions()
+    {
+        CurrentlySelected.ShowSelector(false);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        LastButton = optionsButton;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game!");
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        CurrentlySelected.ShowSelector(false);
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        LastButton = returnButton;
+    }
+
+    public void Dummy()
+    {
+        Debug.Log("Dummy Result!");
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        animator.Play("ShowMainMenu", 0, 0f);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0) || isOnMenu)
+        {
+            return;
+        }
+
+        if (isTitleSequenceFinished)
+        {
+            isOnMenu = true;
+            mainMenu.SetActive(true);
+            defaultMainMenuButton.Select();
+            animator.SetTrigger(ShowMenu);
+        }
+
+        animator.Play("IntroAnimation", 0, 1f);
+    }
+}
