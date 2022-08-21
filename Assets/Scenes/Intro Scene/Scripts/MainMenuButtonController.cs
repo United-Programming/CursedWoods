8f0440c9eb35007393028aa763f0c0af58a62f46
CPU <claudio.pucci@gmail.com>
2022-08-21 15:01:45 +0200

    Improved UI for the intro screen

--- Assets/Scenes/Intro Scene/Scripts/MainMenuButtonController.cs
+++ Assets/Scenes/Intro Scene/Scripts/MainMenuButtonController.cs
@@ -3,55 +3,50 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MainMenuButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler
-{
-    [SerializeField] private EventSystem eventSystem;
-    [SerializeField] private Button button;
-    [SerializeField] private GameObject selector;
-    [SerializeField] private Button defaultButton;
+public class MainMenuButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler {
+  [SerializeField] private EventSystem eventSystem;
+  [SerializeField] private Button button;
+  [SerializeField] private Toggle toggle;
+  [SerializeField] private Slider slider;
+  [SerializeField] private GameObject selector;
+  [SerializeField] private Button defaultButton;
 
-    public void OnSelect(BaseEventData eventData)
-    {
-        if (MainMenuController.CurrentlySelected != null && MainMenuController.CurrentlySelected != this)
-        {
-            MainMenuController.CurrentlySelected.ShowSelector(false);
-        }
-
-        selector.SetActive(true);
-        ShowSelector(true);
-        MainMenuController.CurrentlySelected = this;
+  public void OnSelect(BaseEventData eventData) {
+    if (MainMenuController.CurrentlySelected != null && MainMenuController.CurrentlySelected != this) {
+      MainMenuController.CurrentlySelected.ShowSelector(false);
     }
 
-    public void OnDeselect(BaseEventData eventData)
-    {
-        defaultButton.Select();
-        ShowSelector(false);
+    selector.SetActive(true);
+    ShowSelector(true);
+    MainMenuController.CurrentlySelected = this;
+  }
+
+  public void OnDeselect(BaseEventData eventData) {
+    if (defaultButton != null) defaultButton.Select();
+    ShowSelector(false);
+  }
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    if (button != null) button.Select();
+    if (toggle != null) toggle.Select();
+    if (slider != null) slider.Select();
+  }
+
+  public void ShowSelector(bool boolean) {
+    selector.SetActive(boolean);
+  }
+
+  private IEnumerator CheckSelection() {
+    yield return null;
+    if (eventSystem.currentSelectedGameObject != null &&
+        eventSystem.currentSelectedGameObject != MainMenuController.LastButton) {
+      yield break;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        button.Select();
-    }
+    defaultButton.Select();
+  }
 
-    public void ShowSelector(bool boolean)
-    {
-        selector.SetActive(boolean);
-    }
-
-    private IEnumerator CheckSelection()
-    {
-        yield return null;
-        if (eventSystem.currentSelectedGameObject != null &&
-            eventSystem.currentSelectedGameObject != MainMenuController.LastButton)
-        {
-            yield break;
-        }
-
-        defaultButton.Select();
-    }
-
-    private void OnEnable()
-    {
-        StartCoroutine(CheckSelection());
-    }
+  private void OnEnable() {
+    StartCoroutine(CheckSelection());
+  }
 }
--- Assets/Scenes/Intro Scene/Scripts/MainMenuController.cs
+++ Assets/Scenes/Intro Scene/Scripts/MainMenuController.cs
@@ -2,78 +2,83 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MainMenuController : MonoBehaviour
-{
-    public static MainMenuButtonController CurrentlySelected { get; set; }
+public class MainMenuController : MonoBehaviour {
+  public static MainMenuButtonController CurrentlySelected { get; set; }
 
-    public static GameObject LastButton { get; private set; }
+  public static GameObject LastButton { get; private set; }
 
-    private static readonly int ShowMenu = Animator.StringToHash("showMenu");
+  private static readonly int ShowMenu = Animator.StringToHash("showMenu");
 
-    [SerializeField] private EventSystem eventSystem;
-    [SerializeField] private Animator animator;
-    [SerializeField] private bool isTitleSequenceFinished;
-    [SerializeField] private bool isOnMenu;
-    [SerializeField] private GameObject mainMenu;
-    [SerializeField] private GameObject optionsMenu;
-    [SerializeField] private Button defaultMainMenuButton;
-    [SerializeField] private GameObject optionsButton;
-    [SerializeField] private GameObject returnButton;
+  [SerializeField] private EventSystem eventSystem;
+  [SerializeField] private Animator animator;
+  [SerializeField] private bool isTitleSequenceFinished;
+  [SerializeField] private bool isOnMenu;
+  [SerializeField] private GameObject mainMenu;
+  [SerializeField] private GameObject optionsMenu;
+  [SerializeField] private GameObject creditsMenu;
+  [SerializeField] private Button defaultMainMenuButton;
+  [SerializeField] private GameObject optionsButton;
+  [SerializeField] private GameObject creditsButton;
+  [SerializeField] private GameObject returnButton;
 
-    public void StartNewGame()
-    {
-        Debug.Log("Start a new game!");
-        // starts a new game
+  public void StartNewGame() {
+    Debug.Log("Start a new game!");
+    // starts a new game
+  }
+
+  public void ShowOptions() {
+    CurrentlySelected.ShowSelector(false);
+    mainMenu.SetActive(false);
+    optionsMenu.SetActive(true);
+    LastButton = optionsButton;
+  }
+
+  public void ShowCredits() {
+    CurrentlySelected.ShowSelector(false);
+    mainMenu.SetActive(false);
+    creditsMenu.SetActive(true);
+    LastButton = optionsButton;
+  }
+
+  public void QuitGame() {
+    Debug.Log("Quit game!");
+    Application.Quit();
+  }
+
+  public void ReturnToMainMenu() {
+    CurrentlySelected.ShowSelector(false);
+    optionsMenu.SetActive(false);
+    mainMenu.SetActive(true);
+    LastButton = returnButton;
+  }
+
+  public void Dummy() {
+    Debug.Log("Dummy Result!");
+  }
+
+  public void ShowMainMenu() {
+    mainMenu.SetActive(true);
+    animator.Play("ShowMainMenu", 0, 0f);
+  }
+
+  private void Update() {
+    if (!Input.GetMouseButtonDown(0) || isOnMenu) {
+      return;
     }
 
-    public void ShowOptions()
-    {
-        CurrentlySelected.ShowSelector(false);
-        mainMenu.SetActive(false);
-        optionsMenu.SetActive(true);
-        LastButton = optionsButton;
+    if (isTitleSequenceFinished) {
+      isOnMenu = true;
+      mainMenu.SetActive(true);
+      defaultMainMenuButton.Select();
+      animator.SetTrigger(ShowMenu);
     }
 
-    public void QuitGame()
-    {
-        Debug.Log("Quit game!");
-        Application.Quit();
-    }
+    animator.Play("IntroAnimation", 0, 1f);
+  }
 
-    public void ReturnToMainMenu()
-    {
-        CurrentlySelected.ShowSelector(false);
-        optionsMenu.SetActive(false);
-        mainMenu.SetActive(true);
-        LastButton = returnButton;
-    }
 
-    public void Dummy()
-    {
-        Debug.Log("Dummy Result!");
-    }
-
-    public void ShowMainMenu()
-    {
-        mainMenu.SetActive(true);
-        animator.Play("ShowMainMenu", 0, 0f);
-    }
-
-    private void Update()
-    {
-        if (!Input.GetMouseButtonDown(0) || isOnMenu)
-        {
-            return;
-        }
-
-        if (isTitleSequenceFinished)
-        {
-            isOnMenu = true;
-            mainMenu.SetActive(true);
-            defaultMainMenuButton.Select();
-            animator.SetTrigger(ShowMenu);
-        }
-
-        animator.Play("IntroAnimation", 0, 1f);
-    }
+  public Toggle FullScreenToggle;
+  public void ToggleFullScreen() {
+    Screen.fullScreen = FullScreenToggle.isOn;
+  }
 }
